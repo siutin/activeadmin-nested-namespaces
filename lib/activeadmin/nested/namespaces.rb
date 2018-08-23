d46d29f8ae5f0389194b64bca51afdb6e6a84f73
martin <osiutino@gmail.com>
2018-08-23 12:16:53 +0800

    create project

---
+++ lib/activeadmin/nested/namespaces.rb
@@ -0,0 +1,9 @@
+require "activeadmin/nested/namespaces/version"
+
+module Activeadmin
+  module Nested
+    module Namespaces
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/activeadmin/nested/namespaces/version.rb
@@ -0,0 +1,7 @@
+module Activeadmin
+  module Nested
+    module Namespaces
+      VERSION = "0.1.0"
+    end
+  end
+end
