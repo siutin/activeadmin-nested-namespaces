42582ee9d6e8989a212b4c3316774525ecc97d74
martin <osiutino@gmail.com>
2018-08-23 13:13:09 +0800

    install activeadmin

---
+++ dummy_app/app/models/admin_user.rb
@@ -0,0 +1,10 @@
+class AdminUser < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, 
+         :recoverable, :rememberable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, 
+         :recoverable, :rememberable, :validatable
+end
