6bae0d9c7ff4f8255070842cd73e4854ec4666f3
martin <osiutino@gmail.com>
2018-08-23 13:07:59 +0800

    link dummy app with gem

--- dummy_app/config/application.rb
+++ dummy_app/config/application.rb
@@ -14,5 +14,6 @@ module DummyApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.eager_load_paths += %W( #{config.root}/../lib )
   end
 end
